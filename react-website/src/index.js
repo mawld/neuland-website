64e258a97244612754c4fb1bab3e97cefd22bf06
lina0987 <li.st99@web.de>
2022-11-29 20:03:25 +0200

    struktur verbessert

--- react-website/src/index.js
+++ react-website/src/index.js
@@ -1,28 +1,10 @@
-import React from "react";
-import { Nav, NavLink, NavMenu }
-	from "./NavbarElements";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const Navbar = () => {
-return (
-	<>
-	<Nav>
-		<NavMenu>
-		<NavLink to="/about" activeStyle>
-			About
-		</NavLink>
-		<NavLink to="/contact" activeStyle>
-			Contact Us
-		</NavLink>
-		<NavLink to="/blogs" activeStyle>
-			Blogs
-		</NavLink>
-		<NavLink to="/sign-up" activeStyle>
-			Sign Up
-		</NavLink>
-		</NavMenu>
-	</Nav>
-	</>
+ReactDOM.render(
+<React.StrictMode>
+	<App />
+</React.StrictMode>,
+document.getElementById('root')
 );
-};
-
-export default Navbar;
