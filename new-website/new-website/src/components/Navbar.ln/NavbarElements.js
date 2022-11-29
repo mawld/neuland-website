b30c98590e19c30af64a68ed385cc9f4aa4c60f7
magda <magdalena.waldau@outlook.de>
2022-11-29 20:23:56 +0100

    ?

---
+++ new-website/new-website/src/components/Navbar.ln/NavbarElements.js
@@ -0,0 +1,12 @@
+import { FaBars } from "react-icons/fa";
+import { NavLink as Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+    background: #ffb3ff;
+    height: 85px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0.2rem calc((100vw - 1000px) / 2);
+    z-index: 12;
+`;
