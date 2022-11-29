e1affec40740cc8c08a796a93a33bbb1e0d328a7
lina0987 <li.st99@web.de>
2022-11-29 21:32:27 +0200

    pages code

---
+++ react-website/src/pages/join.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Join = () => {
+return (
+	<div>
+	<h1>Mail us on feedback@geeksforgeeks.org</h1>
+	</div>
+);
+};
+
+export default Join;
---
+++ react-website/src/pages/members.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Members = () => {
+return (
+	<div>
+	<h1>Sign Up Successful</h1>
+	</div>
+);
+};
+
+export default Members;
